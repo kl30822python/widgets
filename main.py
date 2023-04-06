717b505195f8694bfef6ea40c607c13448890bf2
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-04-06 15:17:19 +0800

    update main.py 選單

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
-from parts import TopFrame
+from parts import TopFrame, MediumFrame
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        topFrame = TopFrame(self)        
+        topFrame = TopFrame(self, borderwidth=0)        
         topFrame.pack()
+        mediumFrame = MediumFrame(self,borderwidth=0)
+        mediumFrame.pack(fill=tk.X)
         
 
 def main():
